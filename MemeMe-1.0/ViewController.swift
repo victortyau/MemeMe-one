158885259f63d5bd7856f682f5e2d8681a6a1803
Victor Tejada Yau <victortyau@gmail.com>
2021-06-20 22:46:25 -0500

    working on mememe 1.0 app

--- MemeMe-1.0/ViewController.swift
+++ MemeMe-1.0/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var topMessage: UITextField!
+    @IBOutlet weak var bottomMessage: UITextField!
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.green  /* TODO: fill in appropriate UIColor */,
+        NSAttributedString.Key.foregroundColor: UIColor.blue/* TODO: fill in appropriate UIColor */,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth: 1.0 /* TODO: fill in appropriate Float */
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        topMessage.defaultTextAttributes = memeTextAttributes
+        topMessage.insertText("TOP")
+        topMessage.textAlignment = .center
+        
+        bottomMessage.defaultTextAttributes = memeTextAttributes
+        bottomMessage.insertText("BOTTOM")
+        bottomMessage.textAlignment = .center
     }
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+       
+    }
+    
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+    }
 }
 
